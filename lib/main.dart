80fd1dcdbf8668181d5ee7b14aca80e9514bf299
eticaretci <134627472+eticaretci@users.noreply.github.com>
2026-02-08 23:42:18 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,21 +4,30 @@ import 'package:permission_handler/permission_handler.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await [Permission.camera, Permission.photos, Permission.storage].request();
+
+  await [
+    Permission.camera, 
+    Permission.microphone,
+    Permission.photos, 
+    Permission.storage
+  ].request();
+  
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+  
   @override
   Widget build(BuildContext context) {
-    const String appName = String.fromEnvironment('APP_NAME', defaultValue: 'Maðaza');
-    const String themeColor = String.fromEnvironment('THEME_COLOR', defaultValue: '#007bff');
+    const String appName = String.fromEnvironment('APP_NAME', defaultValue: 'Eticaret Sitesi');
+    const String themeColorHex = String.fromEnvironment('THEME_COLOR', defaultValue: '#000000');
+    
     return MaterialApp(
       title: appName,
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primaryColor: Color(int.parse(themeColor.replaceFirst('#', '0xff'))),
+        primaryColor: Color(int.parse(themeColorHex.replaceFirst('#', '0xff'))),
       ),
       home: const WebViewPage(),
     );
@@ -34,7 +43,8 @@ class WebViewPage extends StatefulWidget {
 class _WebViewPageState extends State<WebViewPage> {
   @override
   Widget build(BuildContext context) {
-    const String siteUrl = String.fromEnvironment('SITE_URL', defaultValue: 'https://google.com');
+    const String siteUrl = String.fromEnvironment('SITE_URL', defaultValue: 'https://www.eticaretsitesisatisi.com');
+    
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
@@ -43,13 +53,22 @@ class _WebViewPageState extends State<WebViewPage> {
           initialSettings: InAppWebViewSettings(
             javaScriptEnabled: true,
             allowsInlineMediaPlayback: true,
-            userAgent: "EticaretSitesiSatisi-App-Wrapper",
+            useOnDownloadStart: true,
+            useOnLongPressRequestOverride: true,
+            userAgent: "Mozilla/5.0 (iPhone; CPU iPhone OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1",
           ),
           onPermissionRequest: (controller, request) async {
             return PermissionResponse(
               resources: request.resources,
               action: PermissionResponseAction.GRANT,
             );
+          },
+          shouldOverrideUrlLoading: (controller, navigationAction) async {
+            var uri = navigationAction.request.url;
+            if (uri != null && !["http", "https", "file", "chrome", "data", "javascript"].contains(uri.scheme)) {
+              return NavigationActionPolicy.CANCEL;
+            }
+            return NavigationActionPolicy.ALLOW;
           },
         ),
       ),
