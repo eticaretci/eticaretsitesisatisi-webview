0b23b6d4f484d11b5a656ab6181fe66b96a6d028
eticaretci <134627472+eticaretci@users.noreply.github.com>
2026-02-10 18:20:50 +0300

    Update MainActivity.java

--- android/app/src/main/java/com/eticaretsitesisatisi/app/MainActivity.java
+++ android/app/src/main/java/com/eticaretsitesisatisi/app/MainActivity.java
@@ -3,4 +3,5 @@ package com.eticaretsitesisatisi.app;
 import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
+    // Saf Java yapısı, Kotlin hatasını kökten çözer.
 }
