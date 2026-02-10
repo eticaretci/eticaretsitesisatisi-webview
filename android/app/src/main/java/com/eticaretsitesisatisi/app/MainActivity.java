ff19c1433d9adfe7120d4b3080ed34e812ab555d
eticaretci <134627472+eticaretci@users.noreply.github.com>
2026-02-10 20:09:34 +0300

    Delete android/app/src/main/java/com/eticaretsitesisatisi/app directory

--- android/app/src/main/java/com/eticaretsitesisatisi/app/MainActivity.java
+++
@@ -1,7 +0,0 @@
-package com.eticaretsitesisatisi.app;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-    // Saf Java yapısı, Kotlin hatasını kökten çözer.
-}
