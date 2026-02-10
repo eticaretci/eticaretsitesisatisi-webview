a10132c59e8554b2cbc3a908dc816ed43b973409
eticaretci <134627472+eticaretci@users.noreply.github.com>
2026-02-10 20:12:20 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,71 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-import 'package:url_launcher/url_launcher.dart';
-
-const String siteUrl = String.fromEnvironment('BASE_URL', defaultValue: 'https://www.eticaretsitesisatisi.com');
-const String themeColorHex = String.fromEnvironment('THEME_COLOR', defaultValue: '#000000');
-const String appName = String.fromEnvironment('APP_NAME', defaultValue: 'EticaretSitesi');
 
 void main() {
-  runApp(const MyApp());
+  // GitHub'dan --dart-define ile gelen verileri yakala
+  const String rawUrl = String.fromEnvironment('BASE_URL', defaultValue: 'https://www.eticaretsitesisatisi.com');
+  const String appTitle = String.fromEnvironment('APP_NAME', defaultValue: 'Eticaret Sitesi');
+  
+  // URL'nin başında http/https yoksa ekle (Hata önleyici)
+  final String finalUrl = rawUrl.startsWith('http') ? rawUrl : 'https://$rawUrl';
+
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: appTitle,
+    theme: ThemeData(useMaterial3: true),
+    home: WebViewContainer(url: finalUrl, title: appTitle),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class WebViewContainer extends StatefulWidget {
+  final String url;
+  final String title;
+  const WebViewContainer({super.key, required this.url, required this.title});
 
   @override
-  Widget build(BuildContext context) {
-    // Renk kodunu işle
-    final Color themeColor = Color(int.parse(themeColorHex.replaceFirst('#', '0xff')));
-
-    return MaterialApp(
-      title: appName,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColor: themeColor,
-        useMaterial3: true, // pubspec'teki modern Flutter yapısına uygun
-      ),
-      home: const WebViewPage(),
-    );
-  }
+  State<WebViewContainer> createState() => _WebViewContainerState();
 }
 
-class WebViewPage extends StatefulWidget {
-  const WebViewPage({super.key});
-
-  @override
-  State<WebViewPage> createState() => _WebViewPageState();
-}
-
-class _WebViewPageState extends State<WebViewPage> {
-  late final WebViewController controller;
+class _WebViewContainerState extends State<WebViewContainer> {
+  late final WebViewController _controller;
 
   @override
   void initState() {
     super.initState();
-    controller = WebViewController()
+    _controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setNavigationDelegate(
-        NavigationDelegate(
-          onNavigationRequest: (NavigationRequest request) async {
-            // WhatsApp, Telefon veya harici linkleri tarayıcıda açar
-            if (!request.url.startsWith('http')) {
-              await launchUrl(Uri.parse(request.url), mode: LaunchMode.externalApplication);
-              return NavigationDecision.prevent;
-            }
-            return NavigationDecision.navigate;
-          },
-        ),
-      )
-      ..loadRequest(Uri.parse(siteUrl));
+      ..setBackgroundColor(Colors.white)
+      ..loadRequest(Uri.parse(widget.url));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: WebViewWidget(controller: controller),
+      appBar: AppBar(
+        title: Text(widget.title),
+        centerTitle: true,
       ),
+      body: WebViewWidget(controller: _controller),
     );
   }
 }
