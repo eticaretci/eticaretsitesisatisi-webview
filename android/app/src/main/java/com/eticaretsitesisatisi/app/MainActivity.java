51d64e0ef54f0e6a5f0849218a92677c837f4725
eticaretci <134627472+eticaretci@users.noreply.github.com>
2026-02-10 18:11:41 +0300

    Update MainActivity.java

--- android/app/src/main/java/com/eticaretsitesisatisi/app/MainActivity.java
+++ android/app/src/main/java/com/eticaretsitesisatisi/app/MainActivity.java
@@ -1,1 +1,8 @@
+package com.eticaretsitesisatisi.app;
 
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+    // Burada ekstra bir kod yazmana gerek yok. 
+    // WebView ve diğer tüm ayarlar Dart tarafında yönetilecek.
+}
