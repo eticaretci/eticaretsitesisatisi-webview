df392f22684d35d2acbd51075507a12fb786f471
eticaretci <134627472+eticaretci@users.noreply.github.com>
2026-02-10 11:49:30 +0300

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/eticaretsitesisatisi/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/eticaretsitesisatisi/app/MainActivity.kt
@@ -1,28 +1,32 @@
 package com.eticaretsitesisatisi.app
 
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.net.http.SslError
 import android.webkit.SslErrorHandler
+import android.view.ViewGroup
+import android.view.View
 
 class MainActivity: FlutterActivity() {
     
     override fun onPostResume() {
         super.onPostResume()
 
-        val view = (window.decorView.findViewById(android.R.id.content) as android.view.ViewGroup).getChildAt(0)
-        
+        val rootView = window.decorView.findViewById<ViewGroup>(android.R.id.content)
+        findAndFixWebView(rootView)
+    }
+
+    private fun findAndFixWebView(view: View) {
         if (view is WebView) {
             view.webViewClient = object : WebViewClient() {
-                override fun onReceivedSslError(
-                    view: WebView?,
-                    handler: SslErrorHandler?,
-                    error: SslError?
-                ) {
-                  handler?.proceed()
+                override fun onReceivedSslError(v: WebView?, handler: SslErrorHandler?, e: SslError?) {
+                    handler?.proceed()
                 }
+            }
+        } else if (view is ViewGroup) {
+            for (i in 0 until view.childCount) {
+                findAndFixWebView(view.getChildAt(i))
             }
         }
     }
