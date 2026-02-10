3ee5355a19dcab2a9b23e7c4db0e889f54ed6994
eticaretci <134627472+eticaretci@users.noreply.github.com>
2026-02-10 11:39:23 +0300

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/eticaretsitesisatisi/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/eticaretsitesisatisi/app/MainActivity.kt
@@ -1,1 +1,29 @@
+package com.eticaretsitesisatisi.app
 
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.net.http.SslError
+import android.webkit.SslErrorHandler
+
+class MainActivity: FlutterActivity() {
+    
+    override fun onPostResume() {
+        super.onPostResume()
+
+        val view = (window.decorView.findViewById(android.R.id.content) as android.view.ViewGroup).getChildAt(0)
+        
+        if (view is WebView) {
+            view.webViewClient = object : WebViewClient() {
+                override fun onReceivedSslError(
+                    view: WebView?,
+                    handler: SslErrorHandler?,
+                    error: SslError?
+                ) {
+                  handler?.proceed()
+                }
+            }
+        }
+    }
+}
