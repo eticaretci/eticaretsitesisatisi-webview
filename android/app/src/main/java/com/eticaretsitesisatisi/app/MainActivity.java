251bf29463e881683e8b4b2d162c1631e92644a9
eticaretci <134627472+eticaretci@users.noreply.github.com>
2026-02-10 20:24:25 +0300

    Update MainActivity.java

--- android/app/src/main/java/com/eticaretsitesisatisi/app/MainActivity.java
+++ android/app/src/main/java/com/eticaretsitesisatisi/app/MainActivity.java
@@ -3,5 +3,5 @@ package com.eticaretsitesisatisi.app;
 import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
-    // İçerisi boş kalabilir, v2 embedding bunu otomatik halleder.
+    // İçerisi tamamen boş olmalı.
 }
